09703d4eab37070e65dd84a4a094ca2df879a03e
Daniele Dell'Aquila <dellaqui@nscl.msu.edu>
2018-11-05 11:13:02 -0500

    Corrected output tree branch HiRA.fKinEnergy

--- E15190ReaderData.cpp
+++ E15190ReaderData.cpp
@@ -179,7 +179,7 @@ void E15190Reader::BuildCalibratedEvent()
       fHiRACalibratedData.fTheta[i]=GetThetaPixel(fHiRACalibratedData.fnumtel[i], fHiRACalibratedData.fnumstripf[i], fHiRACalibratedData.fnumstripb[i]);
       fHiRACalibratedData.fPhi[i]=GetPhiPixel(fHiRACalibratedData.fnumtel[i], fHiRACalibratedData.fnumstripf[i], fHiRACalibratedData.fnumstripb[i]);
       fHiRACalibratedData.fEnergycsiCal[i]=GetCsIEMeV(fHiRACalibratedData.fEnergycsi[i],fHiRACalibratedData.fnumtel[i],fHiRACalibratedData.fnumcsi[i],fHiRACalibratedData.fZ[i],fHiRACalibratedData.fA[i]); //Energy in the CsI
-      fHiRACalibratedData.fKinEnergy[i]=fHiRACalibratedDataBuffer.fEnergySifMatched[i]+fHiRACalibratedData.fEnergycsiCal[i]; //Total kinetic energy as the sum of DE (front) and E
+      fHiRACalibratedData.fKinEnergy[i]=fHiRACalibratedDataBuffer.fEnergySifCal[i]+fHiRACalibratedData.fEnergycsiCal[i]; //Total kinetic energy as the sum of DE (front) and E
       fHiRACalibratedData.fBeta[i]=-9999;
       fHiRACalibratedData.fMomentum[i]=-9999;
     }
